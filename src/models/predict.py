92e0a715b1296255b6666506a06547c90c4fc226
MichalGasiorowski <MGasiorowski82@gmail.com>
2022-08-11 16:41:53 +0200

    predict fixes

--- src/models/predict.py
+++ src/models/predict.py
@@ -10,46 +10,49 @@ from flask import Flask, request, jsonify
 import boto3
 import pickle
 from src.features.build_features import preprocess_test
+from src.features.build_features import all_columns
 
 app = Flask('survivorship-prediction')
 
-RUN_ID = 'd6dc868dda9e41ecaf536a91f41e059e'
+DEFAULT_RUN_ID = 'd6dc868dda9e41ecaf536a91f41e059e'
+RUN_ID = os.getenv('RUN_ID', DEFAULT_RUN_ID)
+BUCKET_NAME = os.getenv('BUCKET_NAME', 'mlflow-enkidupal-experiments')
 
-model_s3 = f's3://mlflow-enkidupal-experiments/1/{RUN_ID}/artifacts/models_pickle/'
 
-model = mlflow.pyfunc.load_model(model_s3)
+model_s3_path = f's3://{BUCKET_NAME}/1/{RUN_ID}/artifacts/models_pickle/'
 
-s3client = boto3.client('s3' )
+model = mlflow.pyfunc.load_model(model_s3_path)
 
-response = s3client.get_object(Bucket='mlflow-enkidupal-experiments', Key=f'1/{RUN_ID}/artifacts/preprocessor/preprocessor.b')
+s3client = boto3.client('s3')
+
+response = s3client.get_object(Bucket=BUCKET_NAME, Key=f'1/{RUN_ID}/artifacts/preprocessor/preprocessor.b')
 
 body = response['Body'].read()
 preprocessor = pickle.loads(body)
 
-def create_feature(json):
-    df = pd.DataFrame(json, index=[0])
+
+def create_features(json):
+    df = pd.json_normalize(json, meta=all_columns)
     return df
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    json = request.get_json()
-    print('json', json)
-    features = create_feature(json)
-    print('features', features)
-
-    return calculate_predict(features)
 
 def calculate_predict(df: pd.DataFrame):
     preprocessed = preprocess_test(df, preprocessor)
 
-    prediction = model.predict(preprocessed)
-
+    predictions = model.predict(preprocessed).tolist()
     result = {
-        'prediction': float(prediction[0])
+        'predictions': list(predictions)
     }
-    print(result)
 
     return jsonify(result)
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    json = request.get_json()
+    features = create_features(json)
+    return calculate_predict(features)
 
 
 if __name__ == "__main__":
--- src/models/test_predict.py
+++ src/models/test_predict.py
@@ -1,14 +1,36 @@
 import requests
 
 passenger_X = {
-    "Sex": ['female'],
-    "Pclass": [0],
-    "Embarked": ['C'],
-    "SibSp": [1],
-    "Parch": [10],
-    "Fare": [69.34]
+    "Sex": 'female',
+    "Pclass": 0,
+    "Embarked": 'C',
+    "SibSp": 1,
+    "Parch": 10,
+    "Fare": 69.34
 }
 
+two_passengers = [
+    {
+        "Sex": 'female',
+        "Pclass": 0,
+        "Embarked": 'C',
+        "SibSp": 1,
+        "Parch": 10,
+        "Fare": 69.34
+    },
+    {
+    "Sex": 'male',
+    "Pclass": 1,
+    "Embarked": 'C',
+    "SibSp": 0,
+    "Parch": 2,
+    "Fare": 8
+}
+]
+
 url = 'http://localhost:9696/predict'
-response = requests.post(url, json=passenger_X)
+response = requests.post(url, json=two_passengers)
+print(response.json())
+
+response = requests.post(url, json=[passenger_X]) # 1-element list, since the list is expected
 print(response.json())
