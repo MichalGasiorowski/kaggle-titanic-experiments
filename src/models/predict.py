272965655c718c15262a09a02ab0944ff08c5993
MichalGasiorowski <MGasiorowski82@gmail.com>
2022-08-19 12:40:24 +0200

    predict script: add predict calculation for csv/parquet files

--- src/models/predict.py
+++ src/models/predict.py
@@ -31,23 +31,31 @@ body = response['Body'].read()
 preprocessor = pickle.loads(body)
 
 
+def load_file_from_s3():
+    pass
+
+def read_data(filename):
+    """Return processed features dict and target."""
+
+    # Load dataset
+    if filename.endswith('parquet'):
+        df = pd.read_parquet(filename, columns=all_columns)
+    elif filename.endswith('csv'):
+        df = pd.read_csv(filename)
+    else:
+        raise "Error: not supported file format."
+
+    return df
+
 def create_features(json):
     df = pd.json_normalize(json, meta=all_columns)
     return df
 
 
-#def calculate_predict(df: pd.DataFrame):
-def calculate_predict(raw_dicts, df: pd.DataFrame):
+def calculate_predict(df: pd.DataFrame):
     X_test = preprocess_test(df, preprocessor)
 
-    ###
-    # returns dictionary, since model expects its
-    # and has dv as first pipeline step
-    #dicts = preprocess_test_no_pipeline(df)
-    ###
     print(model)
-
-    print(f'raw_dicts:{raw_dicts}', type(raw_dicts))
 
     predictions = model.predict(X_test).tolist()
     result = {
@@ -57,17 +65,23 @@ def calculate_predict(raw_dicts, df: pd.DataFrame):
     return jsonify(result)
 
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     json = request.get_json()
-    print(json, type(json))
+    print(json)
     features = create_features(json)
-    return calculate_predict(json, features)
+    return calculate_predict(features)
 
+@app.route('/predict_from_path', methods=['POST'])
+def predict_from_path():
+    json = request.get_json()
+    print(json)
+    path = json['path']
+    df = read_data(path)
+
+    return calculate_predict(df)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9696)
-
 
 
--- src/models/test_predict.py
+++ src/models/test_predict.py
@@ -28,9 +28,20 @@ two_passengers = [
 }
 ]
 
+
+test_path_json = {
+    'path': '../../data/external/test/test.csv'
+}
+
 url = 'http://localhost:9696/predict'
 response = requests.post(url, json=two_passengers)
 print(response.json())
 
 response = requests.post(url, json=[passenger_X]) # 1-element list, since the list is expected
+print(response.json())
+
+
+path_url = 'http://localhost:9696/predict_from_path'
+
+response = requests.post(path_url, json=test_path_json)
 print(response.json())
