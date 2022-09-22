fa073b34adf6e7a46ebbcf6b4538ec8bfdfb40d9
MichalGasiorowski <MGasiorowski82@gmail.com>
2022-09-22 21:35:05 +0200

    changed all Pipfile to python 3.9; added dev dependencies: pylint, black, isort

--- src/models/lambda_function.py
+++ src/models/lambda_function.py
@@ -5,6 +5,7 @@ from src.models.predict import create_features
 
 
 def lambda_handler(event, context):
+    # pylint: disable=unused-argument
     query = event['data']
     features = create_features(query)
     predictions = calculate_predict(features)
